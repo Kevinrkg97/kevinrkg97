6e6331adac692200794319d8c019b436ce5ac3ac
kevinrkg97 <kevinrkg97@gmail.com>
2021-08-02 21:02:20 -0500

    Envio Email

--- src/pages/home/components/contact/contact.jsx
+++ src/pages/home/components/contact/contact.jsx
@@ -1,23 +1,37 @@
 import './contact.css';
+import React from 'react';
+import emailjs from 'emailjs-com';
 
-const Contact = () => {
+export default function Contact (){
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('Portfolio', 'kevinrkg97-contacto', e.target, 'user_eHxQsQ8VDlXV45h08H4w4')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+
+
     return(
         <div className="contact" id="contact">
             <h2>Contáctame</h2>
             <hr />
-            <form>
+            <form onSubmit={sendEmail} >
                 <div className="data">
-                    <input type="text" placeholder="Nombre" />
-                    <input type="number" placeholder="Teléfono" />
-                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Nombre" name="nombre" />
+                    <input type="number" placeholder="Teléfono" name="telefono" />
+                    <input type="email" placeholder="Email" name="email" />
                 </div>
                 <div className="message">
-                    <textarea type="text" placeholder="Escribe tu mensaje" />
-                    <button>Enviar</button>
+                    <textarea type="text" placeholder="Escribe tu mensaje" name="mensaje" />
+                    <input type="submit" value="Enviar" />
                 </div>
             </form>
         </div>
     );
 }
 
-export default Contact;
